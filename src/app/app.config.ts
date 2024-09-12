36cbb8215c1aa1216146196c2327bf0c2ac063ad
Jorge Estébenez <jorgecodesync@gmail.com>
2024-09-12 18:36:59 +0200

    Added socket-io

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -11,25 +11,30 @@ import { provideHttpClient, withInterceptors } from '@angular/common/http';
 import { provideTransloco } from '@ngneat/transloco';
 import { TranslocoHttpLoader } from '../contexts/shared/services/transloco-loader';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
-import { authInterceptor } from '../contexts/shared/interceptors/auth.interceptor';
 import { AuthDataModule } from '../contexts/auth/data/auth-data.module';
+import { SocketIoConfig, SocketIoModule } from 'ngx-socket-io';
+import { environment } from '../environments/environment.prod';
+
+const config: SocketIoConfig = { url: environment.socketUrl, options: {} };
 
 export const appConfig: ApplicationConfig = {
   providers: [
     provideZoneChangeDetection({ eventCoalescing: true }),
     provideHttpClient(/*withInterceptors([authInterceptor])*/),
     provideRouter(routes),
-    importProvidersFrom([BrowserAnimationsModule, AuthDataModule]),
+    importProvidersFrom([
+      BrowserAnimationsModule,
+      AuthDataModule,
+      SocketIoModule.forRoot(config),
+    ]),
     provideTransloco({
       config: {
         availableLangs: ['en', 'es'],
         defaultLang: 'en',
-        // Remove this option if your application doesn't support changing language in runtime.
         reRenderOnLangChange: true,
         prodMode: !isDevMode(),
       },
       loader: TranslocoHttpLoader,
     }),
-    // provideAuthService()
   ],
 };
