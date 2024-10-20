d42f831716e2ceede1d5358e4fc028c35ac30601
Jorge Estébenez <jorgecodesync@gmail.com>
2024-10-20 16:21:07 +0200

    update dockerfile.prod and enviroment files

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,8 +1,5 @@
 export const environment = {
   production: true,
-  // apiUrl: 'http://localhost:3000',
-  // socketUrl: 'ws://localhost:3100',
-
-  apiUrl: 'http://34.111.168.41',
-  socketUrl: 'ws://34.110.188.150',
+  apiUrl: 'https://api.hexitalk.com',
+  socketUrl: 'ws://socket.hexitalk.com',
 };
